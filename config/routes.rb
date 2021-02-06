adccedf84edcb37c5b16e751834e55629490142c
septianmalela <maulanas430@gmail.com>
2021-02-06 14:17:50 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # match '*path' => redirect('/'), via: :get
+
+  root "forum_threads#index"
+  resources :admin do
+    collection do
+     get :table_post 
+     get :table_user 
+     get :table_like 
+    end
+  end
+  
+    resources :users
+    resources :forum_threads, only: [:show, :new, :create] do
+      resources :forum_posts, only: [:create]
+  end
+
+  resources :forum_threads do 
+    resources :likes do member do
+      patch 'like_thread'
+      patch 'dislike_thread'
+    end
+      end
+  end
+
+  resources :forum_threads do member do
+    patch 'true_thread'
+    patch 'false_thread'
+  end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_034611) do
+ActiveRecord::Schema.define(version: 2020_12_19_141346) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
